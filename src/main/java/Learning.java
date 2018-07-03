a7575f2fc4989cc0f0be0c268099468e76d66488
Jagoda <jagodaciniak@gmail.com>
2018-07-03 19:28:28 +0200

    array learn

---
+++ src/main/java/Learning.java
@@ -0,0 +1,10 @@
+
+public class Learning {
+    public static void main(String[] args) {
+        for (int i=10; i>=1; i--){
+            System.out.printf("Bomba wybuchnie za: %d\n", i);
+
+             }
+        System.out.println("BUUUM");
+    }
+}
